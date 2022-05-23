61932c7e29b544ee79e5c69daaf5a8d25f9977ad
mkangelo <mikequi000@gmail.com>
2022-05-22 21:09:45 -0500

    Test Unitario Manual

--- src/test/java/com/mkangelo/javetests/util/StringUtilTest.java
+++ src/test/java/com/mkangelo/javetests/util/StringUtilTest.java
@@ -3,10 +3,15 @@ package com.mkangelo.javetests.util;
 public class StringUtilTest {
     public static void main(String[] args) {
 
-        String result = StringUtil.repeat("Hola", 3);
-        System.out.println(result);
+        String result = StringUtil.repeat("hola", 3);
+        assertEquals(result, "holaholahola");
 
-        String result2 = StringUtil.repeat("hola", 1);
-        System.out.println(result2);
+        assertEquals(StringUtil.repeat("hola", 1), "hola"); // INLINE
+    }
+
+    public static void assertEquals(String actual, String expected){
+        if(!actual.equals(expected)){
+            throw new RuntimeException(actual + " is not equal to " + expected);
+        }
     }
 }
