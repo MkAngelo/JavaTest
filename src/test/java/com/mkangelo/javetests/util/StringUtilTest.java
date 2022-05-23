f7b789b25e454e24d1319ca8d61a22ee7c90a941
mkangelo <mikequi000@gmail.com>
2022-05-22 21:26:08 -0500

    Diferentes tests

--- src/test/java/com/mkangelo/javetests/util/StringUtilTest.java
+++ src/test/java/com/mkangelo/javetests/util/StringUtilTest.java
@@ -5,8 +5,22 @@ import org.junit.Test;
 
 public class StringUtilTest {
     @Test
-    public void testRepeat() {
-        Assert.assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
+    public void repeat_string_once() {
         Assert.assertEquals(StringUtil.repeat("hola", 1), "hola");
+    }
+
+    @Test
+    public void repeat_string_multiple_times() {
+        Assert.assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
+    }
+
+    @Test
+    public void repeat_string_zero_times(){
+        Assert.assertEquals(StringUtil.repeat("hola", 0), "");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times(){
+        StringUtil.repeat("hola", -1);
     }
 }
